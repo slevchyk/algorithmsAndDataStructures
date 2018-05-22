fed11d35897078be1b35b83e6fbd455c62ce45bc
slevchyk <s.levchyk@gmail.com>
2018-05-22 16:55:05 +0300

    goroutine

--- sorting/quickSorting/main.go
+++ sorting/quickSorting/main.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func main() {
 
-	var arr = []int{10, 7, 8, 9, 1, 5}
+	slc := []int{10, 7, 8, 9, 1, 5}
 
 	fmt.Println("raw array")
-	printArray(arr)
+	printArray(slc)
 
-	n := len(arr)
-	sort(arr, 0, n-1)
+	n := len(slc)
+	sort(slc, 0, n-1)
 
 	fmt.Println("sorted array")
-	printArray(arr);
+	printArray(slc);
 
 }
 
@@ -21,18 +21,20 @@ func main() {
 	 arr[] --> Array to be sorted,
 	 low  --> Starting index,
 	 high  --> Ending index */
-func sort(arr []int, low int, high int) {
+func sort(slc []int, low int, high int) {
+
+	var pi int
 
 	if (low < high) {
-		var pi int
+
 		/* pi is partitioning index, arr[pi] is
   		now at right place */
-		pi = partition(arr, low, high)
+		pi = partition(slc, low, high)
 
 		// Recursively sort elements before
 		// partition and after partition
-		sort(arr, low, pi-1)
-		sort(arr, pi+1, high)
+		go sort(slc, low, pi-1)
+		go sort(slc, pi+1, high)
 	}
 }
 
@@ -42,39 +44,37 @@ func sort(arr []int, low int, high int) {
        smaller (smaller than pivot) to left of
        pivot and all greater elements to right
        of pivot */
-func partition(arr []int, low int, high int) int {
+func partition(slc []int, low int, high int) int {
 
 
-	pivot := arr[high]
+	pivot := slc[high]
 	i := low - 1 // index of smaller element
 
 	for j := low; j < high; j++ {
 		// If current element is smaller than or
 		// equal to pivot
-		if arr[j] <= pivot {
+		if slc[j] <= pivot {
 			i++
 
 			// swap arr[i] and arr[j]
-			temp := arr[i]
-			arr[i] = arr[j]
-			arr[j] = temp
+			temp := slc[i]
+			slc[i] = slc[j]
+			slc[j] = temp
 		}
 	}
 
 	// swap arr[i+1] and arr[high] (or pivot)
-	temp := arr[i+1]
-	arr[i + 1] = arr[high]
-	arr[high] = temp
+	temp := slc[i + 1]
+	slc[i + 1] = slc[high]
+	slc[high] = temp
 
 	return i + 1
 }
 
 func printArray(arr []int) {
 
-	n := len(arr)
-
-	for i := 0; i < n; i++ {
-		fmt.Println(i, " - ", arr[i])
+	for key, val := range  arr {
+		fmt.Println(key, " - ", val)
 	}
 	fmt.Println()
 }
